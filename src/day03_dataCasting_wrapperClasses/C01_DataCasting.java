94ef197db33a3ae93d3a72780c17c9f4b7971050
bulutluoz <abbulutluoz@gmail.com>
2024-10-16 20:08:55 +0200

    3.gun sonu

---
+++ src/day03_dataCasting_wrapperClasses/C01_DataCasting.java
@@ -0,0 +1,76 @@
+package day03_dataCasting_wrapperClasses;
+
+public class C01_DataCasting {
+
+
+    public static void main(String[] args) {
+
+        boolean bl = true;
+        char chr = 'a';
+        byte byt = 24;
+        short shrt = 23;
+        int sayiInt = 30;
+        long lng = 40;
+        float flt = 3.5F;
+        double dbl = 4.5;
+        String str = "Java Candir";
+
+
+//         bl = chr;
+//         bl = byt;
+//         bl = sayiInt;
+//         bl = str;
+
+
+//         str = bl;
+//         str = chr;
+//         str = shrt;
+//         str = lng;
+//         str = dbl;
+
+//        byt = shrt;
+//        byt = sayiInt;
+//        byt = lng;
+//        byt = flt;
+//        byt = dbl;
+
+         shrt = byt;
+//        shrt = sayiInt;
+//        shrt = lng;
+//        shrt = flt;
+//        shrt = dbl;
+
+        sayiInt = byt;
+        sayiInt = shrt;
+//        sayiInt = lng;
+//        sayiInt = flt;
+//        sayiInt = dbl;
+
+        lng = byt;
+        lng = shrt;
+        lng = sayiInt;
+//        lng = flt;
+//        lng = dbl;
+
+        flt = byt;
+        flt = shrt;
+        flt = sayiInt;
+        flt = lng;
+//        flt = dbl;
+
+        dbl = byt;
+        dbl = shrt;
+        dbl = sayiInt;
+        dbl = lng;
+        dbl = flt;
+
+
+
+
+
+
+
+
+
+    }
+}
