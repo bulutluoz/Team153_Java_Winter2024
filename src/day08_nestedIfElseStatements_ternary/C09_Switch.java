1695c5a3a3dd0ffec9e3b31b4ae361b4875d2ac7
bulutluoz <abbulutluoz@gmail.com>
2024-10-23 21:45:21 +0200

    08.gun sonu

---
+++ src/day08_nestedIfElseStatements_ternary/C09_Switch.java
@@ -0,0 +1,15 @@
+package day08_nestedIfElseStatements_ternary;
+
+public class C09_Switch {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan bir rakam isteyim ( 0 - 9)
+        // kullanicinin rakam olarak girdigi degeri
+        // metin olarak yazdirin
+        // orn : kullanici 7'ye basarsa, "Girilen rakam : yedi" yazdirin
+
+
+
+    }
+}
