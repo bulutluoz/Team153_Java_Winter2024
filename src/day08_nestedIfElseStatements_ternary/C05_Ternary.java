5aef895a5ad109499ab7a55f7b55c9368b6bcc53
bulutluoz <abbulutluoz@gmail.com>
2024-10-23 20:25:56 +0200

    08.gun sonu

--- src/day08_nestedIfElseStatements_ternary/C05_Ternary.java
+++ src/day08_nestedIfElseStatements_ternary/C05_Ternary.java
@@ -9,14 +9,29 @@ public class C05_Ternary {
         // Kullanicidan iki tamsayi isteyin
 
         // girilen sayilara gore asagidaki degerlendirmeleri yazdirin
-        // 1- sayi1 < sayi2 ise "birinci sayi daha kucuk", degilse "birinci sayi daha kucuk degil"
-        // 2- sayi1 pozitif ise "birinci sayi sifirdan buyuk" , degilse "birinci sayi sifirdan buyuk"
-        // 3- sayi2 50'den buyuk ise "ikinci sayi 50'den buyuk", degilse "ikinci sayi 50'den buyuk degil"
-        // 4- sayi2 5 ile bolunebiliyorsa "ikinci sayi 5'in tam kati", degilse "ikinci sayi 5'in tam kati degil"
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Lutfen 2 tamsayi girin...");
         int sayi1 = scanner.nextInt();
         int sayi2 = scanner.nextInt();
+
+        // 1- sayi1 < sayi2 ise "birinci sayi daha kucuk", degilse "birinci sayi daha kucuk degil"
+
+        System.out.println( sayi1 <sayi2 ? "birinci sayi daha kucuk" : "birinci sayi daha kucuk degil" );
+
+
+        // 2- sayi1 pozitif ise "birinci sayi sifirdan buyuk" , degilse "birinci sayi sifirdan buyuk degil"
+
+        System.out.println( sayi1 > 0 ? "birinci sayi sifirdan buyuk" : "birinci sayi sifirdan buyuk degil" );
+
+
+        // 3- sayi2 50'den buyuk ise "ikinci sayi 50'den buyuk", degilse "ikinci sayi 50'den buyuk degil"
+
+        System.out.println( sayi2 > 50 ? "ikinci sayi 50'den buyuk" : "ikinci sayi 50'den buyuk degil" );
+
+
+        // 4- sayi2 5 ile bolunebiliyorsa "ikinci sayi 5'in tam kati", degilse "ikinci sayi 5'in tam kati degil"
+
+        System.out.println( sayi2 % 5 == 0 ? "ikinci sayi 5'in tam kati" : "ikinci sayi 5'in tam kati degil");
     }
 }
---
+++ src/day08_nestedIfElseStatements_ternary/C06_AtamaOlursa.java
@@ -0,0 +1,14 @@
+package day08_nestedIfElseStatements_ternary;
+
+public class C06_AtamaOlursa {
+
+    public static void main(String[] args) {
+
+        // kullanicidan bir tamsayi isteyin
+
+        // sayi pozitif ise 2 katina cikarin, sayi negatif ise degerini 100 artirin
+
+
+
+    }
+}
