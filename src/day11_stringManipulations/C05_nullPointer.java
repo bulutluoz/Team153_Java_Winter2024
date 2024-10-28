f4fa63117df6dac1b9cac4f355ca05d71c7aa46e
bulutluoz <abbulutluoz@gmail.com>
2024-10-28 20:16:16 +0100

    11.gun sonu

---
+++ src/day11_stringManipulations/C05_nullPointer.java
@@ -0,0 +1,51 @@
+package day11_stringManipulations;
+
+public class C05_nullPointer {
+    public static void main(String[] args) {
+
+        String ogr1 = "Yahya Gulsoy";
+        String ogr2 = "Elif Evsen";
+
+        String ogr3;
+
+        /*
+            bir kod blogunun icinde
+            deger atamadan variable deklare edilebilir
+            ANCAAAAKKK deger atamadan KULLANILAMAZ
+
+            PEKII "" atasak
+
+            "" veya "   " atamak variable'i kullanabilmek icin guzel olur
+            ANCAAAKKK Java bunlari da isim gibi bir deger atanmislar grubuna katar
+            yani "Yahya Gulsoy" atamak ile "" atanmis olmasi Java acisindan birdir
+
+
+            Bize ekstra bir cozum lazim
+
+            hem deger atamayacagiz
+            hem de java yazdirmamiza engel olmayacak
+
+            null bir deger degil pointer'dir
+            null olarak isaretlenen bir variable'a
+            deger atanmadigini Java bilir
+            ama yazdirilmasina da ENGEL OLMAZ
+
+         */
+
+        //System.out.println(ogr3);
+
+        String ogr4 = "";
+        String ogr5 = "  ";
+        String ogr6 = "         ";
+
+        System.out.println(ogr4);
+
+
+        String ogr7 = null ;
+        System.out.println(ogr7); // null
+        System.out.println("ogrenci1 : " + ogr1);
+        System.out.println("ogrenci2 : " + ogr2);
+        System.out.println("ogrenci7 : " + ogr7);
+
+    }
+}
