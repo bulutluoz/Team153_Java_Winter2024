5a29d5a2d4aea9c0cf950a4f203014b539108557
bulutluoz <abbulutluoz@gmail.com>
2024-10-28 19:19:46 +0100

    11.gun sonu

--- src/day11_stringManipulations/C02_lastIndexOf.java
+++ src/day11_stringManipulations/C02_lastIndexOf.java
@@ -4,6 +4,45 @@ public class C02_lastIndexOf {
 
     public static void main(String[] args) {
 
-        String metin = ""
+        String metin = "Bu java halledilecek.";
+
+        // ilk a'nin index'i
+        System.out.println(metin.indexOf('a')); // 4
+
+        // cumlede gecen son a'nin index'i
+        System.out.println(metin.lastIndexOf("a")); // 9
+
+        // a'nin 2. kullanimin index'i
+        int birinciIndexa = metin.indexOf('a');
+        System.out.println(metin.indexOf('a', birinciIndexa+1)); // 6
+
+        // a'nin sondan 2. kullaniminin index'i
+        int aninSonIndexi = metin.lastIndexOf("a");
+        System.out.println( metin.lastIndexOf("a",aninSonIndexi-1)  ); // 6
+
+
+        // java'nin ilk kulaniminin index'i
+        System.out.println(metin.indexOf("java")); // 3
+
+        // java'nin son kulaniminin index'i
+        System.out.println( metin.lastIndexOf("java")); // 3
+
+
+        // "Bu java halledilecek.";
+
+        // h'nin ilk kulaniminin index'i
+        System.out.println(metin.indexOf('h')); // 8
+
+        // h'nin son kulaniminin index'i
+        System.out.println( metin.lastIndexOf("h")); // 8
+
+
+        // Java'nin ilk kulaniminin index'i
+        System.out.println(metin.indexOf("Java")); // -1
+
+
+        // Java'nin son kulaniminin index'i
+        System.out.println(metin.lastIndexOf("Java")); // -1
+
     }
 }
---
+++ src/day11_stringManipulations/C03_KullanimAdediBulma.java
@@ -0,0 +1,13 @@
+package day11_stringManipulations;
+
+public class C03_KullanimAdediBulma {
+
+    public static void main(String[] args) {
+
+        //3- Kullanicidan bir cumle ve aranan bir metin parcasi isteyin
+        //   kullanicinin verdigi metinlere gore asagidaki 3 cumleden birini yazdirin
+        //     - cumle aranan metni icermiyor
+        //     - cumle aranan metni sadece 1 adet iceriyor
+        //     - cumle aranan metni birden fazla iceriyor
+    }
+}
