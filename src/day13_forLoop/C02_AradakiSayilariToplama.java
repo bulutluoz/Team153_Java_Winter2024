ecc6b61d344796d3c7b98a07f397734c92a99873
bulutluoz <abbulutluoz@gmail.com>
2024-10-30 19:18:55 +0100

    13.gun sonu

--- src/day13_forLoop/C02_AradakiSayilariToplama.java
+++ src/day13_forLoop/C02_AradakiSayilariToplama.java
@@ -1,5 +1,7 @@
 package day13_forLoop;
 
+import java.util.Scanner;
+
 public class C02_AradakiSayilariToplama {
 
     public static void main(String[] args) {
@@ -7,6 +9,27 @@ public class C02_AradakiSayilariToplama {
         // Kullanicidan baslangic ve bitis degeri olarak pozitif sayilar alin,
         // sinirlar dahil olarak aralarindaki tum sayilarin toplamini yazdirin.
         // Bitis degeri baslangic degerinden kucukse, uyari yazdirip islemi sonlandirin
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen baslangic icin pozitif bir tamsayi girin");
+        int baslangic = scanner.nextInt();
+
+        System.out.println("Lutfen bitis icin pozitif bir tamsayi girin");
+        int bitis = scanner.nextInt();
+
+        int toplam = 0;
+
+        if (bitis < baslangic){
+            System.out.println("Bitis degeri baslangic degerinden kucuk olamaz...");
+        } else {
+
+            for (int i = baslangic; i <= bitis ; i++) {
+
+                toplam += i;
+            }
+
+            System.out.println(baslangic + " ile " + bitis + " arasindaki sayilarin toplami : " + toplam);
+        }
 
 
     }
---
+++ src/day13_forLoop/C03_AradakiSayilariToplama.java
@@ -0,0 +1,14 @@
+package day13_forLoop;
+
+public class C03_AradakiSayilariToplama {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan baslangic ve bitis degeri olarak pozitif sayilar alin,
+        // sinirlar dahil olarak aralarindaki tum sayilarin toplamini yazdirin.
+        // Bitis degeri baslangic degerinden kucuk olsa da program calissin
+
+
+
+    }
+}
