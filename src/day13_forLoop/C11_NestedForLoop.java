60542e1aa57b640a989a502d5999049145b9a482
bulutluoz <abbulutluoz@gmail.com>
2024-10-30 21:16:20 +0100

    13.gun sonu

---
+++ src/day13_forLoop/C11_NestedForLoop.java
@@ -0,0 +1,35 @@
+package day13_forLoop;
+
+public class C11_NestedForLoop {
+
+    public static void main(String[] args) {
+
+        // for loop kullanarak 1 2 3 4 yazdirin
+        for (int i = 1; i <=4 ; i++) {
+            System.out.print(i*1 + " ");
+        }
+        System.out.println("");
+
+        // for loop kullanarak 2 4 6 8 yazdirin
+        for (int i = 1; i <=4 ; i++) {
+            System.out.print(i*2 + " ");
+        }
+        System.out.println("");
+
+        // for loop kullanarak 3 6 9 12 yazdirin
+        for (int i = 1; i <=4 ; i++) {
+            System.out.print(i*3 + " ");
+        }
+
+        System.out.println("");
+
+        /*
+            for loop kullanarak tek seferde
+            1 2 3 4
+            2 4 6 8
+            3 6 9 12
+            yazdirin
+         */
+
+    }
+}
