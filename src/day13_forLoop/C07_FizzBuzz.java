41f36f5c5372b99374cea338af58d9a54567061a
bulutluoz <abbulutluoz@gmail.com>
2024-10-30 20:20:14 +0100

    13.gun sonu

---
+++ src/day13_forLoop/C07_FizzBuzz.java
@@ -0,0 +1,14 @@
+package day13_forLoop;
+
+public class C07_FizzBuzz {
+    public static void main(String[] args) {
+
+        // Kullanicidan pozitif bir sayi alin,
+        // 1’den baslayarak o sayiya kadar tum tamsayilari yazdirin, sira
+        //	- 3 ile bolunebilen bir sayiya gelirse, sayi yerine fizz
+        //	- 5 ile bolunebilen bir sayiya gelirse sayi yerine buzz
+        //	- hem 3 hem 5 ile bolunebilen bir sayiya gelirse sayi yerine fizzBuzz yazdirin
+
+        // 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzBuzz
+    }
+}
