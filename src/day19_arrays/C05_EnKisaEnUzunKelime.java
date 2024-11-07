57b1bc52af176148107a8ebcfccd9fbc8dde82e9
bulutluoz <abbulutluoz@gmail.com>
2024-11-07 21:03:11 +0100

    day19 sonu

--- src/day19_arrays/C05_EnKisaEnUzunKelime.java
+++ src/day19_arrays/C05_EnKisaEnUzunKelime.java
@@ -8,7 +8,37 @@ public class C05_EnKisaEnUzunKelime {
         // en uzun ve en kisa kelimeleri yazdiran bir method olusturun.
         // Esit uzunluktaki kelimelerden herhangi biri olabilir
 
-        String[] isimler = {"Nihad", "Sumeyra","Yigit","Tugba"};
+        String[] isimler = {"Nihad","Sumeyra","Yigit Cem","Ali","Tugba"};
+
+        enUzunEnKisayiYazdir(isimler);
+
+
+    }
+
+    public static void enUzunEnKisayiYazdir(  String[] kelimeler   ){
+
+        String enKisaKelime = kelimeler[0];
+        String enUzunKelime = kelimeler[0];
+
+
+        for (int i = 0; i < kelimeler.length ; i++) {
+
+            if ( kelimeler[i].length() > enUzunKelime.length() ){
+
+                enUzunKelime = kelimeler[i];
+            }
+
+            if (kelimeler[i].length() < enKisaKelime.length()){
+
+                enKisaKelime = kelimeler[i];
+            }
+
+        }
+
+        System.out.println(
+                "En uzun kelime : " + enUzunKelime +
+                "\nEn kisa kelime : " + enKisaKelime
+        );
 
     }
 
---
+++ src/day19_arrays/C06_KullaniciyaArrayOlusturtma.java
@@ -0,0 +1,60 @@
+package day19_arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class C06_KullaniciyaArrayOlusturtma {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan array’in boyutunu ve elementlerini alip
+        // array’i olusturan ve bize donduren bir method olusturun.
+
+        int[] kullaniciArrayi =intArrayOlustur();
+
+        System.out.println(Arrays.toString(kullaniciArrayi));
+
+        // kullanici array'indeki pozitif tamsayilarin toplamini yazdirin
+
+        System.out.println(C04_PozitifSayilariToplama.pozitifSayilariTopla(kullaniciArrayi));//
+
+    }
+
+
+    public static int[]  intArrayOlustur(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Array'de kac sayi olacagini giriniz...");
+        int arrayLength = scanner.nextInt(); // 5
+
+        int[] arr = new int[arrayLength]; // [0, 0, 0, 0, 0]
+
+        for (int i = 0; i <arrayLength ; i++) {
+
+            System.out.println("Array'in " + i + ".index'i icin bir deger giriniz...");
+            arr[i] = scanner.nextInt();
+
+        }
+
+        return arr;
+    }
+
+    public static String[]  stringArrayOlustur(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Array'de kac sayi olacagini giriniz...");
+        int arrayLength = scanner.nextInt(); // 5
+
+        String[] arr = new String[arrayLength]; // [null, null, null, null, null]
+
+        for (int i = 0; i <arrayLength ; i++) {
+
+            System.out.println("Array'in " + i + ".index'i icin bir deger giriniz...");
+            arr[i] = scanner.nextLine();
+
+        }
+
+        return arr;
+    }
+
+}
