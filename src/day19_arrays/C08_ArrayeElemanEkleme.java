490d0ff66ce827b2e42ea4d5c4e826e1ea2ba755
bulutluoz <abbulutluoz@gmail.com>
2024-11-08 19:02:33 +0100

    Konu20 sonu

--- src/day19_arrays/C08_ArrayeElemanEkleme.java
+++ src/day19_arrays/C08_ArrayeElemanEkleme.java
@@ -1,5 +1,7 @@
 package day19_arrays;
 
+import java.util.Arrays;
+
 public class C08_ArrayeElemanEkleme {
 
     public static void main(String[] args) {
@@ -12,17 +14,13 @@ public class C08_ArrayeElemanEkleme {
         int[] arr = {3,4,5};
         int eklenecekSayi = 8;
 
-
-        // once uzunlugu arr'den 1 fazla olan baska bir array olusturalim
-
-
-        // arr'deki eski elemanlari yeni array'e kopyalayin
-
-
-        // yeni array'in son elemani olarak eklenecek sayiyi atayin
+        arr = arrayeElemanEkle(arr,eklenecekSayi);
+        arr = arrayeElemanEkle(arr,6);
+        arr = arrayeElemanEkle(arr,0);
+        arr = arrayeElemanEkle(arr,3);
 
 
-        // yeni array istenen hale gelince, arr'ye yeniArray degerini atayin
+        System.out.println("arr'nin son hali : " + Arrays.toString(arr));
 
 
 
@@ -31,6 +29,31 @@ public class C08_ArrayeElemanEkleme {
 
     // Verilen int bir array’e
     // istenen bir elemani ekleyip
-    // bize donduren bir method yazin,
-    // eski array’e yeni degeri atayin.
+    // yeni halini bize donduren bir method yazin,
+
+    // [3,4,5,8]
+
+    public static int[] arrayeElemanEkle(  int[] arr , int eklenecekSayi    ){
+
+        // once uzunlugu arr'den 1 fazla olan baska bir array olusturalim
+        int[] yeniArr = new int[arr.length+1]; // [0, 0, 0, 0]
+
+        // arr'deki eski elemanlari yeni array'e kopyalayin
+
+        for (int i = 0; i < arr.length ; i++) {
+
+            yeniArr[i] = arr[i];
+        } // [3, 4, 5, 0]
+
+        // yeni array'in son elemani olarak eklenecek sayiyi atayin
+        yeniArr[yeniArr.length-1] = eklenecekSayi; //  // [3, 4, 5, 8]
+
+        // yeni array istenen hale gelince, arr'ye yeniArray degerini atayin
+        arr = yeniArr;
+
+        return arr;
+
+    }
+
+
 }
---
+++ src/day20_multiDimensionalArrays/C01_KullaniciyaIntArrayOlusturtma.java
@@ -0,0 +1,40 @@
+package day20_multiDimensionalArrays;
+
+import day19_arrays.C08_ArrayeElemanEkleme;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class C01_KullaniciyaIntArrayOlusturtma {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan array’in boyutunu ve elementlerini alip
+        // C08'deki method'u kullanarak array’i olusturun ve yazdirin
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen olusturmak istediginiz array'in uzunlugunu girin");
+
+        int arrayLength = scanner.nextInt(); // 5
+
+        int[] arr = new int[1]; // [0]
+
+        System.out.println("Lutfen array'in ilk elemanini giriniz.."); // 3
+        arr[0] = scanner.nextInt(); // [3]
+
+
+        for (int i = 1 ; i < arrayLength; i++) {
+
+            System.out.println("array'e eklemek uzere bir tamsayi giriniz...");
+
+            int eklenecekSayi = scanner.nextInt();
+
+            arr = C08_ArrayeElemanEkleme.arrayeElemanEkle(arr,eklenecekSayi);
+
+        }
+
+        System.out.println("Olusturdugunuz arr : " + Arrays.toString(arr));
+
+    }
+}
