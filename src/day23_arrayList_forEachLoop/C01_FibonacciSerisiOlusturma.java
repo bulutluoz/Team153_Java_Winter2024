fb14ba7b75fa6a868260d2e850f60a678b8a7788
bulutluoz <abbulutluoz@gmail.com>
2024-11-12 21:17:35 +0100

    22.gun sonu

---
+++ src/day23_arrayList_forEachLoop/C01_FibonacciSerisiOlusturma.java
@@ -0,0 +1,15 @@
+package day23_arrayList_forEachLoop;
+
+public class C01_FibonacciSerisiOlusturma {
+
+    public static void main(String[] args) {
+
+        int n = 15;
+
+        // bize ilk n tane tane Fibonacci sayisini bir list olarak yazdirin
+        // n= 5 icin  ==> 0, 1, 1, 2, 3
+        // n=10 icin ==> 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+
+    }
+}
