e7d0db5d710d0bee06825552da8b8854a414c280
bulutluoz <abbulutluoz@gmail.com>
2024-11-14 19:18:58 +0100

    24.gun sonu

---
+++ src/day24_constructors/C01_Constructor.java
@@ -0,0 +1,54 @@
+package day24_constructors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class C01_Constructor {
+
+
+    public static void main(String[] args) {
+
+
+        List<String> isimler = new ArrayList<>();
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        Random rnd = new Random();
+
+
+        C02_Hemsire hemsire1 = new C02_Hemsire();
+        hemsire1.isim = "Canan";
+        hemsire1.adres = "Balgat";
+
+        System.out.println(hemsire1.isim); // Canan
+        System.out.println(hemsire1.adres); // Balgat
+        System.out.println(hemsire1.tel); // Tel atanmadi
+
+
+            C02_Hemsire    hemsire2    =      new     C02_Hemsire();
+        //  Class ismi    obje ismi  atama  keyword    Constructor
+        // Bir class'dan obje olusturulup ilk deger atamasi yapmak icin
+        // MUTLAKA bir constructor CALISMALIDIR
+
+        hemsire2.isim = "Sumeyra";
+
+
+        System.out.println(  new C02_Hemsire().isim  );
+        // 40.satirda new keyword'u ile yeni bir obje olusturulur
+        // sadece o satirda bu obje kullanilabilir
+        // ama bir variable'a kaydedilmedigi icin
+        // (esitligin sol tarafi olmadigi icin)
+        // bu obje o satirda kullanilir ve sonra cop olur
+
+
+        C02_Hemsire hemsire3;
+
+
+
+
+    }
+}
---
+++ src/day24_constructors/C02_Hemsire.java
@@ -0,0 +1,16 @@
+package day24_constructors;
+
+
+
+
+public class C02_Hemsire {
+
+
+    String isim = "Isim atanmadi";
+    String adres = "Adres atanmadi";
+    String tel = "Tel atanmadi";
+
+
+
+
+}
