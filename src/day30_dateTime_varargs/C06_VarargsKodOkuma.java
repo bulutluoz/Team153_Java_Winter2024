4a0a51e3ddf5ef8476ab630bd3f4a8fdd7457c99
bulutluoz <abbulutluoz@gmail.com>
2024-11-25 20:19:21 +0100

    Day30 sonu

---
+++ src/day30_dateTime_varargs/C06_VarargsKodOkuma.java
@@ -0,0 +1,28 @@
+package day30_dateTime_varargs;
+
+public class C06_VarargsKodOkuma {
+
+    public static void main(String[] args) {
+
+        elemanYazdir("Ali",2,3,4,5,6,7); // 4
+
+        elemanYazdir("Tugba",1,2,3,4,5,6,7); // 5
+
+        ornek("a","b","c","d"); // 4
+
+        ornek("Ali","Veli"); //
+
+
+
+    }
+
+    public static void elemanYazdir(String s , int... v){
+
+        System.out.println( v[s.length()-1] );
+
+    }
+
+    public static void ornek(String s , String... k){
+        System.out.println( s.length() + k.length );
+    }
+}
