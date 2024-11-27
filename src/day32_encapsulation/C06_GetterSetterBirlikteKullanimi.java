ea6feb6d7cab61f7f14baec151fede37f77d03da
bulutluoz <abbulutluoz@gmail.com>
2024-11-27 20:22:15 +0100

    Day32 sonu

---
+++ src/day32_encapsulation/C06_GetterSetterBirlikteKullanimi.java
@@ -0,0 +1,19 @@
+package day32_encapsulation;
+
+public class C06_GetterSetterBirlikteKullanimi {
+
+    public int sayiPublic = 34;
+
+
+
+
+    private int sayiPrivate = 45;
+
+    public int getSayiPrivate() {
+        return sayiPrivate;
+    }
+
+    public void setSayiPrivate(int sayiPrivate) {
+        this.sayiPrivate = sayiPrivate;
+    }
+}
