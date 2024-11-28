f10b03a6bd8d48132fa807267c7d1bcf29510d32
bulutluoz <abbulutluoz@gmail.com>
2024-11-28 21:19:40 +0100

    Day33 sonu

--- src/day33_Inheritance/C04_ParentOlmakIstemeyenClass.java
+++ src/day33_Inheritance/C04_ParentOlmakIstemeyenClass.java
@@ -19,6 +19,13 @@ public final class C04_ParentOlmakIstemeyenClass {
 
     // String, List  ve Scanner Class midir ? yoksa data turu mudur ?
 
+    // String, List  ve Scanner birer Class'dir
+    // ama ayni zamanda str'in data turu String,
+    // sayilar'in data turu List
+    // ve scanner objesinin data turu Scanner'dir
+
+    // Class'lar o class'dan olusturulan objeler icin data turu olurlar.
+
 
 
 }
