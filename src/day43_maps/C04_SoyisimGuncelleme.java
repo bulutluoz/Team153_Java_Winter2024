19d724cffe55d75bcbe802b20b591a32ecaf93a6
bulutluoz <abbulutluoz@gmail.com>
2024-12-13 19:24:02 +0100

    Ders43 sonu

---
+++ src/day43_maps/C04_SoyisimGuncelleme.java
@@ -0,0 +1,27 @@
+package day43_maps;
+
+public class C04_SoyisimGuncelleme extends MapDepo{
+
+    public static void main(String[] args) {
+
+        // numarasi verilen ogrencinin
+        // soyismini verilen yeniSoyisim olarak update edin
+
+        int ogrenciNo = 102;
+        String yeniSoyisim = "Yilmaz";
+
+        //1.adim ogrenciNo ile value'ye ulas
+        String eskiValue = ogrenciMap.get(ogrenciNo); // Veli-Cem-10-K-TM
+
+        //2.adim bilgilere erismek icin split
+        String[] eskiValueArr = eskiValue.split("-"); //  [Veli, Cem, 10, K, TM]
+
+        //3.adim artik soyismi guncelleyebiliriz
+        eskiValueArr[1] = yeniSoyisim; //  [Veli, Yilmaz, 10, K, TM]
+
+        //4.adim array'de yaptigimiz guncellemeyi map'e islemeliyiz
+
+
+
+    }
+}
