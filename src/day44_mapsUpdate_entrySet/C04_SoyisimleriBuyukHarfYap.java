9ddd9014c4e2f7d004b3fe9d695207848f86bdc5
bulutluoz <abbulutluoz@gmail.com>
2024-12-14 16:57:02 +0100

    Ders44 sonu

--- src/day44_mapsUpdate_entrySet/C04_SoyisimleriBuyukHarfYap.java
+++ src/day44_mapsUpdate_entrySet/C04_SoyisimleriBuyukHarfYap.java
@@ -1,12 +1,44 @@
 package day44_mapsUpdate_entrySet;
 
-public class C04_SoyisimleriBuyukHarfYap {
+import day43_maps.MapDepo;
+
+import java.util.Set;
+
+public class C04_SoyisimleriBuyukHarfYap extends MapDepo {
 
     public static void main(String[] args) {
 
         // tum ogrencilerin soyisimlerini
         // tamamen buyuk harf olarak guncelleyin
 
+
+        // 1.adim tum key'leri kaydet
+        Set<Integer> ogrenciKeySeti = ogrenciMap.keySet();
+
+        // 2.adim herbir key'i ele almak icin for-each loop kullan
+
+        for (Integer eachKey : ogrenciKeySeti){
+
+            // 3.adim ogrenci no ile value'ye ulasabiliriz
+            String eachValue = ogrenciMap.get(eachKey);
+
+            // 4.adim bilgilere erisebilmek icin value'yu split ederiz
+            String[] eachValueArr = eachValue.split("-"); // [Ali, Can, 11, H, MF]
+
+            // 5.adim istenen bilgileri update et
+            // tum ogrencilerin soyisimlerini
+            // tamamen buyuk harf olarak guncelleyin
+            eachValueArr[1] = eachValueArr[1].toUpperCase();
+
+            // 6.adim degisiklikler array'de yapildi, array'i birlestirip yeni value'yu bulalim
+            String yeniValue = String.join("-",eachValueArr);
+
+            // 7.adim ogrenciNo ve yeniValue ile ogrenciMap'i update et
+            ogrenciMap.put(eachKey,yeniValue);
+
+        }
+
+        System.out.println(ogrenciMap);
 
     }
 }
---
+++ src/day44_mapsUpdate_entrySet/NestedMapDepo.java
@@ -0,0 +1,45 @@
+package day44_mapsUpdate_entrySet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NestedMapDepo {
+
+    /*
+        sinifMap= { 101={Isim=Ali, soyisim=Can, sinif=10, sube=H, bolum=MF },
+
+     */
+    static protected Map<Integer, Map<String,String> > ogrenciNestedMap = new HashMap<>();
+
+    static {
+
+        Map<String,String> ogrenci101Map = new HashMap<>();
+        ogrenci101Map.put("isim","Ali");
+        ogrenci101Map.put("soyisim","Can");
+        ogrenci101Map.put("sinif","11");
+        ogrenci101Map.put("sube","H");
+        ogrenci101Map.put("bolum","MF");
+
+        ogrenciNestedMap.put(101,ogrenci101Map);
+
+        mapeOgrenciEkle(102,"Veli","Cem","10","K","TM");
+        mapeOgrenciEkle(103,"Ali","Cem","11","K","TM");
+        mapeOgrenciEkle(104,"Ayse","Can","10","H","MF");
+        mapeOgrenciEkle(105,"Sevgi","Cem","11","M","TM");
+        mapeOgrenciEkle(106,"Sevgi","Can","10","K","MF");
+        mapeOgrenciEkle(107,"Esra","Han","11","M","SOZ");
+
+    }
+
+    public static void mapeOgrenciEkle(int ogrNo, String isim, String soyisim, String sinif, String sube,String bolum){
+        Map<String,String> eklenecekOgrenciMap = new HashMap<>();
+        eklenecekOgrenciMap.put("isim",isim);
+        eklenecekOgrenciMap.put("soyisim",soyisim);
+        eklenecekOgrenciMap.put("sinif",sinif);
+        eklenecekOgrenciMap.put("sube",sube);
+        eklenecekOgrenciMap.put("bolum",bolum);
+
+        ogrenciNestedMap.put(ogrNo,eklenecekOgrenciMap);
+
+    }
+}
