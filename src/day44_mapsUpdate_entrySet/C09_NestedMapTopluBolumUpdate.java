0fb1ca06cad9fa0c2153553efcc003135ac09b8a
bulutluoz <abbulutluoz@gmail.com>
2024-12-14 17:53:23 +0100

    Ders44 sonu

---
+++ src/day44_mapsUpdate_entrySet/C09_NestedMapTopluBolumUpdate.java
@@ -0,0 +1,31 @@
+package day44_mapsUpdate_entrySet;
+
+import java.util.Set;
+
+public class C09_NestedMapTopluBolumUpdate extends NestedMapDepo{
+
+    public static void main(String[] args) {
+
+        // tum ogrencileri gozden gecirip
+        // eski bolumu verilen ogrencilerin bolumunu
+        // verilen yeni bolum ile update edin
+
+        String eskiBolum = "MF";
+        String yeniBolum = "Say";
+
+        Set<Integer> ogrenciKeySeti = ogrenciNestedMap.keySet();
+
+        for (Integer eachKey :ogrenciKeySeti){
+
+            String ogrenciBolum = ogrenciNestedMap.get(eachKey).get("bolum");
+
+            if (ogrenciBolum.equalsIgnoreCase(eskiBolum)){
+                ogrenciNestedMap.get(eachKey).replace("bolum",yeniBolum);
+            }
+
+        }
+
+        System.out.println(ogrenciNestedMap);
+
+    }
+}
